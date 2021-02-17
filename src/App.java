eba22688cd906806b202b05a79944400c6d5f385
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-17 19:29:57 +0100

    familiada

--- src/App.java
+++ src/App.java
@@ -6,39 +6,57 @@ public class App {
 
         Scanner Scanner = new Scanner(System.in);
         
-        System.out.println("podaj liczbe a");
-        int a = Scanner.nextInt();
+        int wynik = 0;
+         // Zadeklaruj wartości
+      String[][] dane = {
+        {"Więcej niż jedno zwierzę","stado","klucz","dwa","owca","lama"},
+        {"Sporty na s","sztafeta","skok o tyczce", "skoki narciarskie", "sumo", "szachy"}
+      };
 
-        Scanner.nextLine();
-        System.out.println("podaj znak +, -, /, * lub %");
-        String znak = Scanner.nextLine();
-
-        System.out.println("podaj liczbe b");
-        int b = Scanner.nextInt();
+      // zadeklaruj wynik
+      
+      // Pętla wykonująca się tyle razy, ile jest pytań
+      for(int i = 0; i < dane.length; i++){
         
+        // Wyświetl pytanie (pierwszy element każdej tablicy)
+        System.out.println(dane[i][0]); 
         
-        switch(znak){
-            case "+":
-                System.out.println(a+" + "+b+" = "+(a+b));
-            break;
-            case "-":
-                System.out.println(a+" - "+b+" = "+(a-b));
-            break;
-            case "*":
-                System.out.println(a+" * "+b+" = "+(a*b));
-            break;
-            case "/":
-                System.out.println(a+" / "+b+" = "+(a/b));
-            case "%":
-                System.out.println("reszta z dzielenia wynosi: "+(a%b));
-            break;
-            default:
+        // pobierz odpowiedź
+        String odp = Scanner.nextLine(); 
+        boolean correct = false;
+        // Pętla sprawdzająca odpowiedzi 
+        for(int it = 1; it < dane[i].length; it++){
 
-                System.out.println("Podałeś zły znak");
-            break;
+            
+        
+          
+          // System.out.println("ODP: "+dane[i][it]); // Wyświetl każdą odpowiedź
+
+          // Sprawdź, czy odpowiedź użytkownika znajduje się w tablicy
+          // dodaj punkty do wyniku
+            if(odp.equals(dane[i][it])){
+
+                System.out.println("BRZDĘK");
+                correct = true;
+                wynik += it*10;
+              
+            }
+
+
+          }
+
+          
+        if(!correct){
+            System.out.println("X");
+        }else{
+
         }
         
-        Scanner.close();
+    }
+    System.out.println("Wynik to: "+wynik); // wyświetl wynik
+      
+        
+    Scanner.close();
         
         
     } 
