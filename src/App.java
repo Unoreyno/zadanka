cce5593361995315f9b62f678a8116a6f124d97f
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-18 18:54:52 +0100

    poprawka wojewodztw

--- src/App.java
+++ src/App.java
@@ -8,50 +8,8 @@ public class App {
 
       Scanner Scanner = new Scanner(System.in);
        
-      /*
-       //zadanko ze zgadywankiem
       
-      boolean s =(false);
 
-      int liczba = (int)(Math.random() * 100);
-
-      System.out.println("Masz 5 prób na zgadnięcie wylosowanej liczby");
-
-      // System.out.println(liczba); 
-
-        int proba = 1;
-
-
-        while(proba < 6){
-          
-          System.out.println("To twoja "+proba+" próba, podaj liczbę");
-          int zgad = Scanner.nextInt();
-          if(zgad < liczba){
-            System.out.println("Niestety podałeś za małą liczbę");
-          }else if(zgad > liczba){
-            System.out.println("Niestety podałeś za dużą liczbę");
-          }else if(zgad == liczba){
-            s =true; 
-            break;
-          }
-          proba++;
-        }
-
-        if(s){
-          System.out.println("Brawo, zgadłeś");
-        }else{
-          System.out.println("Niestety nie zgadłeś, liczba to "+liczba);
-        }
-
-      */
-
-    
-
-        
-    
-        
-        
-        
           System.out.println("Podaj nazwę województwa");
           
           System.out.println(szukanie(Scanner.nextLine()));
